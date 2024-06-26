f82a965abaaf8a6dff9de9e578570cf9f51c8d42
Haafii <hafismuhammedtk@gmail.com>
2024-06-26 19:21:38 +0530

    db connected

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,18 @@
-console.log('hi')
+const express  = require('express');
+const dotenv = require("dotenv").config();
+const cors = require('cors');
+const connectDb = require('./config/dbConnection');
+
+
+
+connectDb();
+const app = express();
+const port =  8501;
+
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
+})
