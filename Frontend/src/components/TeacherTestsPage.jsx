67381d0535f1718bdbde4b9293bb2070c608e7ea
Haafii <hafismuhammedtk@gmail.com>
2024-06-28 01:22:05 +0530

    Refactor TeacherTestsPage component UI and styling

--- Frontend/src/components/TeacherTestsPage.jsx
+++ Frontend/src/components/TeacherTestsPage.jsx
@@ -18,13 +18,13 @@ const TeacherTestsPage = () => {
   }, [username]);
 
   return (
-    <div className="p-4 bg-white rounded-lg shadow-md max-w-md mx-auto mt-4">
-      <h2 className="text-2xl font-bold mb-4">Created Tests</h2>
+    <div className="p-4 bg-primary flex flex-col items-center justify-center  shadow-md h-screen">
+      <h2 className="text-2xl font-bold mb-4 text-white">Created Tests</h2>
       {tests.map(test => (
-        <div key={test._id} className="mb-4">
+        <div key={test._id} className="mb-4 flex shadow-md bg-secondary px-4 py-6 rounded-lg w-3/6 items-center justify-center">
           <button
             onClick={() => window.location.href = `/teacher/test/${test._id}`}
-            className="text-blue-500 hover:underline"
+            className="bg-sky-500 hover:bg-sky-600 items-center w-3/6 justify-center text-secondary font-medium py-2 px-4 rounded-lg transition duration-300"
           >
             {test.title}
           </button>
